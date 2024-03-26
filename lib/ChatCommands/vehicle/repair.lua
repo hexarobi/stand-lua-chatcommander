82099203ae706d0fb5e9d459ea1839688145d829
Tyler Chamberlain <hexarobi@gmail.com>
2024-03-26 11:46:33 -0500

    Update repair.lua

--- lib/ChatCommands/vehicle/repair.lua
+++ lib/ChatCommands/vehicle/repair.lua
@@ -11,6 +11,12 @@ return {
         local vehicle = vehicle_utils.get_player_vehicle_in_control(pid)
         if vehicle then
             VEHICLE.SET_VEHICLE_FIXED(vehicle)
+            -- Also repair vehicle if its been destroyed by water
+            VEHICLE.SET_VEHICLE_DIRT_LEVEL(vehicle, 0.0)
+            VEHICLE.SET_VEHICLE_UNDRIVEABLE(vehicle, false)
+            VEHICLE.SET_VEHICLE_ENGINE_HEALTH(vehicle, 1000.0)
+            VEHICLE.SET_VEHICLE_PETROL_TANK_HEALTH(vehicle, 1000.0)
+            VEHICLE.SET_VEHICLE_ENGINE_ON(vehicle, true, true, true)
             cc_utils.help_message(pid, "Vehicle repaired")
         end
     end
