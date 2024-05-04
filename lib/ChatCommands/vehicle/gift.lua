40fd57daac45f9248bb942b07728f8dbc726e806
Tyler Chamberlain <hexarobi@gmail.com>
2024-05-04 12:47:45 -0500

    Update gift.lua

--- lib/ChatCommands/vehicle/gift.lua
+++ lib/ChatCommands/vehicle/gift.lua
@@ -23,6 +23,23 @@ local function gift_vehicle_to_player(pid, vehicle)
     DECORATOR.DECOR_SET_INT(vehicle, "Veh_Modded_By_Player", pid_hash)
 end
 
+-- Delete any invisible cars that are commonly left over from gifting. Credit to Holy for finding this check
+local function clear_invisible_vehicles(pid, range)
+    if range == nil then range = 50 end
+    local player_pos = ENTITY.GET_ENTITY_COORDS(PLAYER.GET_PLAYER_PED_SCRIPT_INDEX(pid), 1)
+    for _, vehicle_handle in entities.get_all_vehicles_as_handles() do
+        local entity_pos = ENTITY.GET_ENTITY_COORDS(vehicle_handle, 1)
+        local dist = SYSTEM.VDIST(player_pos.x, player_pos.y, player_pos.z, entity_pos.x, entity_pos.y, entity_pos.z)
+        if dist <= range then
+            if vehicle_handle ~= -1 and not ENTITY.IS_ENTITY_VISIBLE(vehicle_handle) then
+                local vehicle_name = VEHICLE.GET_DISPLAY_NAME_FROM_VEHICLE_MODEL(ENTITY.GET_ENTITY_MODEL(vehicle_handle))
+                cc_utils.debug_log("Deleting invisible vehicle: "..vehicle_name)
+                entities.delete(vehicle_handle)
+            end
+        end
+    end
+end
+
 return {
     command="gift",
     group="vehicle",
@@ -32,6 +49,7 @@ return {
     execute=function(pid)
         local vehicle = vehicle_utils.get_player_vehicle_in_control(pid)
         if vehicle_utils.is_vehicle_command_ready(pid, vehicle) then
+            clear_invisible_vehicles(pid)
             gift_vehicle_to_player(pid, vehicle)
             cc_utils.help_message(pid, "You may now park this car in a full garage and permanently replace another car. For more help say !help gift")
         end
