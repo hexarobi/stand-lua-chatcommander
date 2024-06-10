9ae9891f5be95aacb6fc984dad69ef6d2576db4a
Tyler Chamberlain <hexarobi@gmail.com>
2024-06-10 07:44:17 -0500

    update vip

--- lib/ChatCommands/player/vip.lua
+++ lib/ChatCommands/player/vip.lua
@@ -48,6 +48,12 @@ return {
             memory.read_int(memory.script_global(1916087 + 9)), -- f_8
             memory.read_int(memory.script_global(1916087 + 10)), -- f_9
         })
-        cc_utils.help_message(pid, "Org invite sent. Please check your phone to accept invite.")
+        if players.get_org_type(players.user()) == 0 then
+            -- CEO Org
+            cc_utils.help_message(pid, "Org invite sent. For VIP access, please accept the invite on your phones SecuroServ app.")
+        else
+            -- Motorcycle Club
+            cc_utils.help_message(pid, "MC invite sent. For VIP access, please accept the invite on your phones Job List app.")
+        end
     end
 }
